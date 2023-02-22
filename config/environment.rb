04292c790ec6a37cf9407d8d1c64a5b9eb9431bf
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-02-22 14:26:30 +0300

    Understanding ORMS

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,10 @@ Bundler.require
 
 require_relative '../lib/song'
 
+# It is the work of the program as a whole to be able to creat and establish the database
+
+
+#The DB is the constant that is equal to the hash that tends to contain our connection to the database.
+
+
 DB = { conn: SQLite3::Database.new("db/music.db") }
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
 class Song
 
+  #The setter and getter methods for name and album 
   attr_accessor :name, :album
+
+  #WE have a constructor that tends to create an instance of the name and the album.
 
   def initialize(name:, album:)
     @name = name
@@ -8,3 +11,5 @@ class Song
   end
 
 end
+# It is not the work of the song class to create the database.
+# The classes are mapped into tables that are inside the database not the database as a whole.
